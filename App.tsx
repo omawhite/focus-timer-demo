5019ec42b288230f2f394d2f2d7f25464ba76756
Omar White <omawhite@indiana.edu>
2021-02-21 12:50:33 -0500

    adds code to app component from other repo

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
 import { DripsyProvider, SafeAreaView, P, Container, Button, Box, Row } from 'dripsy';
 import { StatusBar } from 'expo-status-bar';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Alert, Platform, StyleSheet } from 'react-native';
+
+enum TimerMode {
+  POMODORO = 'Pomodoro',
+  SHORT_BREAK = 'Short Break',
+  LONG_BREAK = 'Long Break',
+}
+
+const timerModeValueMap = {
+  [TimerMode.POMODORO]: 25,
+  [TimerMode.SHORT_BREAK]: 5,
+  [TimerMode.LONG_BREAK]: 10,
+};
 
 const theme = {};
 
 export default function App() {
+  const [timerMode, setTimerMode] = useState(TimerMode.POMODORO);
   const onPressStart = useCallback(() => {
+    const message = `set timer for ${timerModeValueMap[timerMode]} minutes`;
     if (Platform.OS === 'web') {
-      return console.log('You pressed start');
+      return console.info(message);
     }
-    return Alert.alert('You pressed start');
+    return Alert.alert(message);
   }, []);
   return (
     <DripsyProvider theme={theme}>
@@ -22,11 +36,17 @@ export default function App() {
             height: 'auto',
           }}>
           <Row>
-            <Button title="one" onPress={() => null} />
-            <Button title="two" onPress={() => null} />
-            <Button title="three" onPress={() => null} />
+            <Button title={TimerMode.POMODORO} onPress={() => setTimerMode(TimerMode.POMODORO)} />
+            <Button
+              title={TimerMode.SHORT_BREAK}
+              onPress={() => setTimerMode(TimerMode.SHORT_BREAK)}
+            />
+            <Button
+              title={TimerMode.LONG_BREAK}
+              onPress={() => setTimerMode(TimerMode.LONG_BREAK)}
+            />
           </Row>
-          <P sx={{ textAlign: 'center' }}>25:00</P>
+          <P sx={{ textAlign: 'center' }}>{`${timerModeValueMap[timerMode]}`}</P>
           <Button title={'Start'} onPress={onPressStart} />
         </Box>
         <P>Time to work!</P>
